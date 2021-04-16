197f10dce2e686724136d58d0b7b522187cc5480
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-04-16 15:59:50 +0100

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,18 @@
+pkg update
+pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+pkg install nano
+pkg install nmap
+pkg install php
+pkg install curl
+pkg install wget
+pkg install openssh
+pkg install perl
+pip install requests
+pip install mechanize
+pip install selenium
+pkg install figlet
+pip install bs4
+pip install colorama
